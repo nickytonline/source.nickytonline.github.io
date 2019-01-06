b9b6346162eea38b746d4cc0693e9841a0591ccd
Nick Taylor <nick@iamdeveloper.com>
2019-01-06 13:33:56 -0500

    Added Google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,5 +93,19 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-55732414-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 };
