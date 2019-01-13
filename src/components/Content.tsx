12a1f3060b79a3ef5d5db5869c606a5da69f6609
Nick Taylor <nick@iamdeveloper.com>
2019-01-13 16:48:58 -0500

    Converted <Content /> component to TypeScript.

---
+++ src/components/Content.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export type ContentProps = {
+  content: React.ReactNode;
+  className: string;
+};
+
+export type HtmlContentProps = {
+  content: string;
+  className: string;
+};
+
+export const HTMLContent: React.FC<HtmlContentProps> = ({
+  content,
+  className,
+}) => (
+  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
+);
+
+const Content: React.FC<ContentProps> = ({ content, className }) => (
+  <div className={className}>{content}</div>
+);
+
+export default Content;
