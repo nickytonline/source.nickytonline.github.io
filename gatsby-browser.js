779a17716fd6af28eb72e779eb5a194abe91d6ca
Nick Taylor <nick@iamdeveloper.com>
2019-01-23 23:05:25 -0500

    Adding log message to ensure entry point is running.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,6 @@ require('prismjs/themes/prism-solarizedlight.css');
 
 // This stub is required so that the CSS loads when the site is built for production.
 // https://github.com/gatsbyjs/gatsby/issues/5997#issuecomment-402356717
-exports.onClientEntry = () => {};
+exports.onClientEntry = () => {
+    console.log('All systems go!'); // eslint-disable-line no-console
+};
