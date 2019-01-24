d64f77c25c152fcc47dbaf8ae44d04bf72d69e97
Nick Taylor <nick@iamdeveloper.com>
2019-01-23 23:28:28 -0500

    Fixed prismjs when in prod build mode take 4.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-
 import { Navbar } from 'components';
-
-import './all.sass';
 import { SiteMetaData } from 'types/site-meta-data';
 import { ChildrenProp } from 'types/children-prop';
+
+import './all.sass';
+import 'prismjs/themes/prism-solarizedlight.css';
 
 export type LayoutProps = ChildrenProp<JSX.Element>;
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+    <StaticQuery
+        query={graphql`
       query HeadingQuery {
         site {
           siteMetadata {
@@ -49,42 +49,42 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => (
         }
       }
     `}
-    render={(data: { site: { siteMetadata: SiteMetaData } }) => (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta
-            name="description"
-            content={data.site.siteMetadata.description}
-          />
+        render={(data: { site: { siteMetadata: SiteMetaData } }) => (
+            <div>
+                <Helmet>
+                    <html lang="en" />
+                    <title>{data.site.siteMetadata.title}</title>
+                    <meta
+                        name="description"
+                        content={data.site.siteMetadata.description}
+                    />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/img/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/img/favicon-32x32.png"
-            sizes="32x32"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/img/favicon-16x16.png"
-            sizes="16x16"
-          />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/img/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        href="/img/favicon-32x32.png"
+                        sizes="32x32"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        href="/img/favicon-16x16.png"
+                        sizes="16x16"
+                    />
 
-          <meta name="theme-color" content="#fff" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
-        </Helmet>
-        <Navbar siteMetadata={data.site.siteMetadata} />
-        <div>{children}</div>
-      </div>
-    )}
-  />
+                    <meta name="theme-color" content="#fff" />
+                    <meta property="og:title" content={data.site.siteMetadata.title} />
+                    <meta property="og:url" content="/" />
+                    <meta property="og:image" content="/img/og-image.jpg" />
+                </Helmet>
+                <Navbar siteMetadata={data.site.siteMetadata} />
+                <div>{children}</div>
+            </div>
+        )}
+    />
 );
