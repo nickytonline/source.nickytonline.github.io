e96ec0dd2d1f958900b25d86efb15511b63c55fe
Nick Taylor <nick@iamdeveloper.com>
2019-01-23 22:52:34 -0500

    Fixed prismjs when in prod build mode.

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -6,84 +6,85 @@ import { Content, HTMLContent, Layout, BlogTag, PageTitle } from 'components';
 import { MarkdownRemark } from 'types/markdown-remark';
 import { PageTemplateProps } from './page-template-props';
 import styles from './blog-post.module.scss';
+import 'prismjs/themes/prism-solarizedlight.css';
 
 export type BlogPostTemplateProps = PageTemplateProps & {
-  description: string;
-  tags: string[];
-  date: string;
-  helmet?: React.ReactNode;
+    description: string;
+    tags: string[];
+    date: string;
+    helmet?: React.ReactNode;
 };
 
 export const BlogPostTemplate: React.FC<BlogPostTemplateProps> = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  date,
-  helmet = '',
+    content,
+    contentComponent,
+    description,
+    tags,
+    title,
+    date,
+    helmet = '',
 }) => {
-  const PostContent = contentComponent || Content;
+    const PostContent = contentComponent || Content;
 
-  return (
-    <section className="section">
-      {helmet}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className={styles.postDate}>{date}</div>
-            <PageTitle>{title}</PageTitle>
-            <div className={styles.postDescription}>{description}</div>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div className={styles.tagContainer}>
-                <h4>Tags</h4>
-                <ul className={styles.tagList}>
-                  {tags.map(tag => (
-                    <li key={`${tag}tag`}>
-                      <BlogTag url={`/tags/${kebabCase(tag)}/`} name={tag} />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="section">
+            {helmet}
+            <div className="container content">
+                <div className="columns">
+                    <div className="column is-10 is-offset-1">
+                        <div className={styles.postDate}>{date}</div>
+                        <PageTitle>{title}</PageTitle>
+                        <div className={styles.postDescription}>{description}</div>
+                        <PostContent content={content} />
+                        {tags && tags.length ? (
+                            <div className={styles.tagContainer}>
+                                <h4>Tags</h4>
+                                <ul className={styles.tagList}>
+                                    {tags.map(tag => (
+                                        <li key={`${tag}tag`}>
+                                            <BlogTag url={`/tags/${kebabCase(tag)}/`} name={tag} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : null}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
-export type BlogPostProps = {
-  data: {
-    markdownRemark: MarkdownRemark;
-  };
-};
+export interface BlogPostProps {
+    data: {
+        markdownRemark: MarkdownRemark;
+    };
+}
 
 const BlogPost: React.FC<BlogPostProps> = ({ data }) => {
-  const { markdownRemark: post } = data;
+    const { markdownRemark: post } = data;
 
-  return (
-    <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        date={post.frontmatter.date}
-        helmet={
-          <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
+    return (
+        <Layout>
+            <BlogPostTemplate
+                content={post.html}
+                contentComponent={HTMLContent}
+                description={post.frontmatter.description}
+                date={post.frontmatter.date}
+                helmet={
+                    <Helmet titleTemplate="%s | Blog">
+                        <title>{`${post.frontmatter.title}`}</title>
+                        <meta
+                            name="description"
+                            content={`${post.frontmatter.description}`}
+                        />
+                    </Helmet>
+                }
+                tags={post.frontmatter.tags}
+                title={post.frontmatter.title}
             />
-          </Helmet>
-        }
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
-      />
-    </Layout>
-  );
+        </Layout>
+    );
 };
 
 // Leaving this as a default export because it's instantiated via gatsby-node.js
