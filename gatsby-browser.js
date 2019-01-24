5eeb8a84b66d49fc359262e9efd2b70209bc4ea8
Nick Taylor <nick@iamdeveloper.com>
2019-01-23 23:20:25 -0500

    Fixed prismjs when in prod build mode take 3.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
-require('prismjs/themes/prism-solarizedlight.css');
-
-// This stub is required so that the CSS loads when the site is built for production.
-// https://github.com/gatsbyjs/gatsby/issues/5997#issuecomment-402356717
-exports.onClientEntry = () => {
-    console.log('All systems go!'); // eslint-disable-line no-console
-};
+import 'prismjs/themes/prism-solarizedlight.css';
