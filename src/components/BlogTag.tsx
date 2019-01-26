4519676181c15d17cb1e60c99128945e83f05b3a
Nick Taylor <nick@iamdeveloper.com>
2019-01-26 17:10:39 -0500

    Style tweaks to links.

--- src/components/BlogTag.tsx
+++ src/components/BlogTag.tsx
@@ -2,17 +2,18 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import styles from './BlogTag.module.scss';
 
-export type BlogTagProps = {
-  name: string;
-  url: string;
-  totalCount?: number;
-};
+export interface BlogTagProps {
+    name: string;
+    url: string;
+    totalCount?: number;
+}
 
 export const BlogTag: React.FC<BlogTagProps> = ({ name, url, totalCount }) => (
-  <Link to={url} className={styles.tag}>
-    {name}
-    {typeof totalCount !== undefined ? (
-      <span className={styles.count}>{totalCount}</span>
-    ) : null}
-  </Link>
+    <Link
+        to={url}
+        className={styles.tag}
+        data-count={typeof totalCount !== undefined ? totalCount : ''}
+    >
+        {name}
+    </Link>
 );
