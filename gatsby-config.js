50d3bab2e07ddeeab78b7cbbc3d271621ee12c51
Nick Taylor <nick@iamdeveloper.com>
2019-10-10 22:38:14 -0400

    Added the  gatsby-remark-social-cards plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,7 @@ module.exports = {
             resolve: 'gatsby-transformer-remark',
             options: {
                 plugins: [
+                    'gatsby-remark-social-cards',
                     {
                         resolve: 'gatsby-remark-relative-images',
                         options: {
